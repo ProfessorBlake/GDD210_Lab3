8ecaf752087f2f85c048c30d439f649fab4563c9
Blake <telefragent@gmail.com>
2019-08-26 18:42:15 -0400

    Initial commit

---
+++ Assets/Example/Scripts/1.Instantiation/InstantiationController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InstantiationController : MonoBehaviour
+{
+	public GameObject[] BallPrefabs;
+	public Camera Cam;
+
+	private void Update()
+	{
+		//Get the mouse position so we can instantiate our prefabs there.
+		Vector3 position = Cam.ScreenToWorldPoint(Input.mousePosition);
+		position.z = 0f; //Set z position to 0.
+
+		//Left mouse button will instantiate the prefab in BallPrefabs at index 0.
+		//Instantiate needs a GameObject to copy (our prefab), a position to place it, and a rotation. Quaternion.Identity is the default rotation.
+		if (Input.GetMouseButtonDown(0))
+		{
+			Instantiate(BallPrefabs[0], position, Quaternion.identity);
+		}
+
+		//Right mouse button will instantiate the prefab in BallPrefabs at index 1;
+		if (Input.GetMouseButtonDown(1))
+		{
+			Instantiate(BallPrefabs[1], position, Quaternion.identity);
+		}
+
+		//Middle mouse button will instantiate a random prefab from the array.
+		if (Input.GetMouseButtonDown(2))
+		{
+			Instantiate(BallPrefabs[Random.Range(0, BallPrefabs.Length)], position, Quaternion.identity);
+		}
+	}
+}
